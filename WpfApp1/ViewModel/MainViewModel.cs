5d929b78e2eb47a3528ec5e635331f89baef4263
nvdat1998 <nguyenvudat1998@gmail.com>
2021-10-20 17:31:38 +0700

    first commit

---
+++ WpfApp1/ViewModel/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace WpfApp1.ViewModel
+{
+  public class MainViewModel : BaseViewModel 
+  {
+    public MainViewModel()
+    {
+      MessageBox.Show("xin chao cac ban");
+    }
+  }
+}
