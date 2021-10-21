b7704848fd34e359c57e1f86006705d6e74b0407
nvdat1998 <nguyenvudat1998@gmail.com>
2021-10-21 13:09:34 +0700

    update

--- WpfApp1/View/New.xaml.cs
+++ WpfApp1/View/New.xaml.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,17 +11,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace WpfApp1.View
 {
   /// <summary>
-  /// Interaction logic for MainWindow.xaml
+  /// Interaction logic for New.xaml
   /// </summary>
-  public partial class MainWindow : Window
+  public partial class New : Page
   {
-    public MainWindow()
+    public New()
     {
       InitializeComponent();
-
     }
   }
 }
---
+++ WpfApp1/ViewModel/ListView_Control.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp1.ViewModel
+{
+  public class ListView_Control
+  {
+
+  }
+}
+
