57245ba406a04bda663c7c8525964ba00b24dfae
nvdat1998 <nguyenvudat1998@gmail.com>
2021-10-21 18:35:42 +0700

    update

---
+++ DemoWPF/Model/DataProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoWPF.Model
+{
+  public class DataProvider
+  {
+    private static DataProvider _ins;
+    public static DataProvider Ins
+    {
+      get { 
+        if (_ins == null) _ins = new DataProvider();
+        return _ins;
+      }
+      set
+      {
+        _ins = value;
+      }
+    }
+    public dempwpfEntities DB { get; set; }
+    private DataProvider()
+    {
+      DB = new dempwpfEntities();
+    }
+    
+  }
+}
---
+++ DemoWPF/ViewModel/MainViewModel.cs
@@ -0,0 +1,78 @@
+﻿using DemoWPF.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApp1.ViewModel
+{
+  public class MainViewModel : BaseViewModel
+  {
+    public ICommand AddEvent { get; set; }
+    private ObservableCollection<item> _List;
+    public ObservableCollection<item> List
+    {
+      get
+      {
+        return _List;
+      }
+      set
+      {
+        _List = value;
+        OnPropertyChanged();
+      }
+    }
+    private string _content;
+    public string Content
+    {
+      get
+      {
+        return _content;
+      }
+      set
+      {
+        this._content = value;
+        OnPropertyChanged();
+      }
+    }
+
+    private string _create;
+    public string create
+    {
+      get
+      {
+        return _create;
+      }
+      set
+      {
+        _create = value;
+        OnPropertyChanged();
+      }
+    }
+
+    private item _SelectedItem;
+    public item SelectedItem
+    {
+      get
+      { 
+        return _SelectedItem;
+      }
+      set
+      {
+        _SelectedItem = value;
+        OnPropertyChanged();
+        if (_SelectedItem != null)
+        {
+          Content = SelectedItem.Content;
+          create = SelectedItem.Created.ToString();
+        }
+      }
+    }
+    public MainViewModel()
+    {
+      List = new ObservableCollection<item>(DataProvider.Ins.DB.items);
+    }
+  }
+}
